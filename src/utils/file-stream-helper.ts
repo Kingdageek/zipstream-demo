f010b6ff00cf0f70f4380c76e1f3c1d504eada16
Nonso Mgbechi <mgbechinonso@gmail.com>
2024-12-08 20:16:58 +0100

    added method to stream zip to temp dir

--- src/utils/file-stream-helper.ts
+++ src/utils/file-stream-helper.ts
@@ -1,10 +1,17 @@
 import { Readable } from 'stream';
 import archiver from 'archiver';
 import { StreamableFile } from '@nestjs/common';
+import { tmpdir } from 'os';
+import { createReadStream, createWriteStream } from 'fs';
 
 export interface ZipData {
   fileStream: Readable;
   fileName: string;
+}
+
+interface ZipStreamResult {
+  size: number;
+  location: string;
 }
 
 export class FileStreamHelper {
@@ -29,5 +36,35 @@ export class FileStreamHelper {
 
     archive.finalize();
     return streamableFile;
+  }
+
+  static streamZipToTemp(zipDataArr: ZipData[]): Promise<ZipStreamResult> {
+    return new Promise((resolve, reject) => {
+      const tmpDir = tmpdir();
+      const location = `${tmpDir}/${Date.now()}.zip`;
+      // write the zip stream to the tempdir to get the final size
+      const destStream = createWriteStream(location);
+      const archive = archiver('zip');
+
+      archive.pipe(destStream);
+
+      zipDataArr.forEach(({ fileName, fileStream }) =>
+        archive.append(fileStream, { name: fileName }),
+      );
+
+      archive.on('end', () => {
+        const result: ZipStreamResult = {
+          location: location,
+          size: archive.pointer(),
+        };
+        resolve(result);
+      });
+      archive.on('error', (err) => {
+        console.error('error from archiver', err);
+        reject(err);
+      });
+
+      archive.finalize();
+    });
   }
 }
